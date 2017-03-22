498b66dd930890276dc003c2ee5d061c84293ec3
Waleed Khan <me@waleedkhan.name>
2017-03-21 19:24:18 -0700

    Add strokes meter
    This adds a meter that tells you how many strokes, on average, you take
    to write words. If you write out many words, this number will be higher;
    likewise, if you write words in relatively few strokes, this number will
    be lower.
    
    This commit also adds a new method of counting words, which is to count
    the number of space-delimited substrings.

--- test/test_plover_wpm_meter.py
+++ test/test_plover_wpm_meter.py
@@ -1,31 +1,34 @@
 import mock
 
-from plover_wpm_meter import _timestamp_chars, _filter_old_chars, _wpm_of_chars
+from plover_wpm_meter import _timestamp_items, _filter_old_items, _wpm_of_chars
 
 
 @mock.patch("time.time")
-def test_timestamp_chars(time):
+def test_timestamp_items(time):
     time.return_value = 1
-    assert _timestamp_chars("foo") == [("f", 1), ("o", 1), ("o", 1)]
+    assert _timestamp_items("foo") == [("f", 1), ("o", 1), ("o", 1)]
 
 
 @mock.patch("time.time")
-def test_filter_old_chars(time):
+def test_filter_old_items(time):
     time.return_value = 20
     chars = [("a", 0), ("b", 10), ("c", 20)]
-    assert _filter_old_chars(chars, 10) == [("b", 10), ("c", 20)]
+    assert _filter_old_items(chars, 10) == [("b", 10), ("c", 20)]
 
 
 def test_wpm_of_nothing():
-    assert _wpm_of_chars(_timestamp_chars("")) == 0
+    assert _wpm_of_chars(_timestamp_items(""), method="traditional") == 0
 
 
 @mock.patch("time.time")
 def test_wpm_of_chars(time):
     time.return_value = 10
     chars = [("f", 5), ("o", 5), ("o", 5), ("o", 5), ("o", 5)]
-    assert _wpm_of_chars(chars) == 12
+    assert _wpm_of_chars(chars, method="traditional") == 12
 
 
 def test_wpm_when_starting_up():
-    assert _wpm_of_chars(_timestamp_chars("foooo barr")) == 120
+    assert _wpm_of_chars(
+        _timestamp_items("foooo barr"),
+        method="traditional",
+    ) == 120
